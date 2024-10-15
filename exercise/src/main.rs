99569ae86af9771ba463c305f1865187201cf3d6
Rabbi Hasan <jasrabbi50@gmail.com>
2024-10-15 19:01:29 +0600

    rust variables, types, values learning

---
+++ exercise/src/main.rs
@@ -0,0 +1,71 @@
+fn main() {
+    println!("Hello 🌍!");
+    println!("Edit me!");
+
+    let mut x: i32 = 10;
+    println!("x: {x}");
+
+    // let x = 20;
+    x = 20;
+    println!("x: {x}");
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}", s2);
+
+    let x = 5;
+    let y = x;
+    println!("x: {}, y: {}", x, y);
+
+    // immutable reference
+    let x = 5;
+    let y = &x;
+    println!("x: {}, y: {}", x, y);
+
+    // mutable reference
+    let mut x = 5;
+    let y = &mut x;
+    *y += 1;
+    println!("x: {}", x);
+
+    // constant variables
+    const MAX_POINTS: u32 = 100_000;
+    println!("{}", MAX_POINTS);
+
+    //arithmetic operation
+    println!("result: {}", interproduct(120, 100, 248));
+
+
+    let a = 10;
+    let b = 20;
+    takes_u32(a);
+    takes_i8(b);
+    // takes_u32(b); error
+
+    let n = 20;
+    println!("fb({n}) = {}", fib(n));
+}
+
+
+
+fn interproduct(a: i32, b: i32, c: i32) -> i32 {
+    return a * b + b * c + c * a;
+}
+
+
+fn takes_u32(x: u32) {
+    println!("u32: {x}");
+}
+
+fn takes_i8(y: i8) {
+    println!("i8: {y}");
+}
+
+
+fn fib(n: u32) -> u32 {
+    if n < 2 {
+        return n;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
